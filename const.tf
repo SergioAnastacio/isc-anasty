4af4bcbe4bfeed04a8911eb3f42c852071ca30f8
SergioAngulo <sergioangulorosales@gmail.com>
2024-12-09 03:26:11 -0600

    Create ISC , Pipeline , Ansible. TODO:Config Secrets

---
+++ const.tf
@@ -0,0 +1,96 @@
+//archivo de variables
+variable "region" {
+  description = "AWS region"
+  type = object({
+    region = string
+  })
+  default = {
+    region = "us-west-1"
+  }
+}
+
+variable "key_name"{
+  description = "value of the key"
+  type = string
+  default = "keymaster"
+}
+
+variable "vpc" {
+  description = "VPC configuration"
+  type = object({
+    cidr_block = string
+  })
+  default = {
+    cidr_block = "10.0.0.0/16"
+  }
+}
+
+variable "subnet" {
+  description = "Subnet configuration"
+  type = object({
+    cidr_block = string
+  })
+  default = {
+    cidr_block = "10.0.1.0/24"
+  }
+}
+variable "route_table" {
+  description = "Route table configuration"
+  type = object({
+    vpc_id = string
+  })
+  default = {
+    vpc_id = "vpc-12345678"
+  }
+}
+variable "table_association" {
+  description = "Route table association configuration"
+  type = object({
+    subnet_id      = string
+    route_table_id = string
+  })
+  default = {
+    subnet_id      = "subnet-12345678"
+    route_table_id = "rtb-12345678"
+  } 
+}
+
+variable "security_group" {
+  description = "Security group configuration"
+  type = object({
+    name = string
+  })
+  default = {
+    name = "example"
+  }
+}
+
+variable "security_group_rule" {
+  description = "Security group rule configuration"
+  type = object({
+    type              = string
+    from_port         = number
+    to_port           = number
+    protocol          = string
+    cidr_blocks       = list(string)
+  })
+  default = {
+    type              = "ingress"
+    from_port         = 3000
+    to_port           = 3000
+    protocol          = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
+}
+
+variable "ec2" {
+  description = "EC2 instance configuration"
+  type = object({
+    ami           = string
+    instance_type = string
+  })
+  default = {
+    ami           = "ami-0c55b159cbfafe1f0"
+    instance_type = "t2.micro"
+  }
+}
---
+++ main.tf
@@ -0,0 +1,57 @@
+terraform {
+  backend "s3" {
+    bucket         = "isc-anasty-state"
+    key            = "terraform.tfstate"
+    region         = var.region.region
+    dynamodb_table = "anasty-lock-table"
+  }
+}
+
+provider "aws" {
+  region = var.region.region
+}
+
+resource "aws_vpc" "anasty_vpc" {
+  cidr_block = var.vpc.cidr_block
+}
+
+resource "aws_subnet" "anasty_subnet" {
+  vpc_id     = aws_vpc.anasty_vpc.id
+  cidr_block = var.subnet.cidr_block
+}
+
+resource "aws_route_table" "anasty_route_table" {
+  vpc_id = aws_vpc.anasty_vpc.id
+}
+
+resource "aws_route_table_association" "anasty_table_association" {
+  subnet_id      = aws_subnet.anasty_subnet.id
+  route_table_id = aws_route_table.anasty_route_table.id
+}
+
+resource "aws_security_group" "anasty_security_group" {
+  vpc_id = aws_vpc.anasty_vpc.id
+}
+
+resource "aws_security_group_rule" "anasty_vpc_sg_rule" {
+  security_group_id = aws_security_group.anasty_security_group.id
+  type              = var.security_group_rule.type
+  from_port         = var.security_group_rule.from_port
+  to_port           = var.security_group_rule.to_port
+  protocol          = var.security_group_rule.protocol
+  cidr_blocks       = var.security_group_rule.cidr_blocks
+}
+
+resource "aws_instance" "back_anasty" {
+  ami           = var.ec2.ami
+  instance_type = var.ec2.instance_type
+  vpc_security_group_ids = [aws_security_group.anasty_security_group.id]
+  key_name               = var.key_name
+}
+resource "aws_eip" "back_anasty_eip" {
+  instance = aws_instance.back_anasty.id
+}
+
+output "instance_ip" {
+  value = aws_eip.back_anasty_eip.public_ip
+}
