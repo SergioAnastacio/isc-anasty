db4041f5d0ec47066cb344bee66e52ad8800e6a6
SergioAngulo <sergioangulorosales@gmail.com>
2024-12-09 20:22:01 -0600

    define ingress 80 443 22

--- main.tf
+++ main.tf
@@ -65,6 +65,24 @@ resource "aws_security_group_rule" "allow_ssh" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
+resource "aws_security_group_rule" "allow_http" {
+  security_group_id = aws_security_group.anasty_security_group.id
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_https" {
+  security_group_id = aws_security_group.anasty_security_group.id
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
 resource "aws_instance" "back_anasty" {
   ami                    = "ami-0657605d763ac72a8"
   instance_type          = "t2.micro"
